f2a8b679fc2214c64b7bf5a05a002ea66403c818
jeremieFrc <ferlayjeremie@gmail.com>
2018-11-01 10:08:37 +0100

    dev and mise en place objet bdd v2.3 and add nom entities

--- src/main/java/com/societe/projet/databases/dto/ArmePhysiqueDTO.java
+++ src/main/java/com/societe/projet/databases/dto/ArmePhysiqueDTO.java
@@ -3,21 +3,26 @@ package com.societe.projet.databases.dto;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ArmePhysiqueDTO implements DTO<ArmePhysiqueDTO> {
+import com.societe.projet.databases.contracts.armes.ArmeContract;
+import com.societe.projet.entities.armes.ArmePhysique;
 
-	
-	
-	
+public class ArmePhysiqueDTO implements DTO<ArmePhysique> {
+
 	@Override
-	public String javaToMySQL(ArmePhysiqueDTO item) {
+	public String javaToMySQL(ArmePhysique item) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ArmePhysiqueDTO mySQLToJava(ResultSet rs) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public ArmePhysique mySQLToJava(ResultSet rs) throws SQLException {
+		ArmePhysique arme = new ArmePhysique();
+		
+		arme.setNom(rs.getString(rs.findColumn(ArmeContract.FIELDS[1])));
+		arme.setDegat(rs.getInt(rs.findColumn(ArmeContract.FIELDS[2])));
+		arme.setCpa(rs.getInt(rs.findColumn(ArmeContract.FIELDS[3])));
+		
+		return arme;
 	}
 
 	
